0de0eaa791a3779e0364f1f431af7a7831f3cb41
Troy <troycoenradie@gmail.com>
2024-12-22 23:38:46 +0100

    bugg fixes nor al working app now

--- server/db.js
+++ server/db.js
@@ -5,4 +5,16 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     logging: false
 });
 
+// Test de connectie
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Database connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+testConnection();
+
 module.exports = sequelize;
