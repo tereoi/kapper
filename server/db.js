549819f43c1ca85d35a091a7ecd6f377659cd448
Troy <troycoenradie@gmail.com>
2024-12-26 22:11:07 +0100

    test3

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: true
+        rejectUnauthorized: false
       }
     }
   });
