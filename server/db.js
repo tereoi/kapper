71217823be5fe717289af59e1087f081bc7c930c
Troy <troycoenradie@gmail.com>
2024-12-26 21:49:51 +0100

    test2

--- server/db.js
+++ server/db.js
@@ -2,9 +2,13 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false
-});
-
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true
+      }
+    }
+  });
 // Test de connectie
 async function testConnection() {
     try {
